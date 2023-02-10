c1b6594dcea397a72f901c62971a09b69e85c895
xpa <xpadev@gmail.com>
2023-02-10 19:50:04 +0900

    fix/callback: useEffect dependencies

--- src/pages/callback.tsx
+++ src/pages/callback.tsx
@@ -11,7 +11,7 @@ const Callback = () => {
         setMessage(`encountered the following error:\n${e}`);
       }
     }
-  },[])
+  },[setMessage])
   return <>
     {message && <pre><code>{message}</code></pre>}
   </>;
