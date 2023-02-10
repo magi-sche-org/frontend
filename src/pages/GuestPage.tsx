a9b753526883a72f8e9ed62ad61cbb2fe3425838
Yuma Satake <satous.y@icloud.com>
2023-02-10 19:29:37 +0900

    GuestPage追加

---
+++ src/pages/GuestPage.tsx
@@ -0,0 +1,13 @@
+import GuestPageBody from "@/components/GuestPage/GuestPageBody";
+import { SecondaryHeader } from "@/components/SecondaryHeader";
+
+const GuestPage = () => {
+	return (
+		<>
+			<SecondaryHeader />
+			<GuestPageBody />
+		</>
+	);
+};
+
+export default GuestPage;
