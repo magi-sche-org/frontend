ac66241c063b7f88118aa87182f6b81e12046e99
Yuta123456 <yuuta09090530@icloud.com>
2023-02-11 02:08:53 +0900

    恐らくgrpcとenvoyは出来てる気がする

--- src/components/TopPage/EventList.tsx
+++ src/components/TopPage/EventList.tsx
@@ -3,6 +3,8 @@ import { mockEventList } from "./data";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import { FC } from "react";
+import { EventClient } from "@/service/api-client/protocol/EventServiceClientPb";
+import { GetEventRequest } from "@/service/api-client/protocol/event_pb";
 export type Event = {
   id: number;
   name: string;
@@ -11,12 +13,20 @@ export type Event = {
 export const EventList = () => {
   // TODO: LocalStorageから取得
   const data = mockEventList;
+  const client = new EventClient("http://localhost:9000", null, null);
+  const req = new GetEventRequest();
+  req.setId("hoge");
+  req.setToken("fuga");
+  const eventData = client.getEvent(req, null).then((res) => {
+    console.log(res);
+    return res.getId();
+  });
   return (
     <>
       {data.map((event) => {
         return (
           <Box marginBottom={"10px"}>
-            <EventCard {...event}></EventCard>
+            <EventCard {...event} key={event.id}></EventCard>
           </Box>
         );
       })}
