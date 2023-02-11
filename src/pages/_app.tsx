047330d59cea1d5a239e243abd6b5c7e1da608f3
Yuma Satake <satous.y@icloud.com>
2023-02-12 01:12:29 +0900

    ヘッダー出し分け

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,37 +1,37 @@
 import { Header } from "@/components/Header";
+import { SecondaryHeader } from "@/components/SecondaryHeader";
 import "@/styles/globals.css";
-import { createTheme, ThemeProvider } from "@mui/material";
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import { SnackbarProvider } from "notistack";
 
 export default function App({ Component, pageProps }: AppProps) {
   const mainTheme = createTheme({
     typography: {
-      fontFamily: [
-        "Roboto",
-        '"Noto Sans JP"',
-        '"Helvetica"',
-        "Arial",
-        "sans-serif",
-      ].join(","),
+      fontFamily: ['"Noto Sans"', '"Helvetica"', "Arial"].join(",")
     },
     palette: {
       primary: {
         main: "#006A71",
-        light: "#46989F",
+        light: "#46989F"
       },
       secondary: {
-        main: "#FFFFDD",
-      },
-    },
+        main: "#FFFFDD"
+      }
+    }
   });
+
+  const router = useRouter();
 
   return (
     <>
       <ThemeProvider theme={mainTheme}>
         <SnackbarProvider>
-          <Header />
+          {router.asPath === "/" ? <Header /> : <></>}
+          {router.asPath.indexOf("guest") === 1 ? <SecondaryHeader /> : <></>}
           <Component {...pageProps} />
+          <CssBaseline />
         </SnackbarProvider>
       </ThemeProvider>
     </>
