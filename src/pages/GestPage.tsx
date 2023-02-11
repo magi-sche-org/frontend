fff2c8bf45bbf29bd0707ba613204f304fb453e4
Yuma Satake <satous.y@icloud.com>
2023-02-11 21:57:26 +0900

    ヘッダー出し分け

--- src/pages/GestPage.tsx
+++ src/pages/GestPage.tsx
@@ -4,7 +4,6 @@ import { SecondaryHeader } from "@/components/SecondaryHeader";
 const GestPage = () => {
 	return (
 		<>
-			<SecondaryHeader />
 			<GestPageBody />
 		</>
 	);
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
 import { Header } from "@/components/Header";
+import { SecondaryHeader } from "@/components/SecondaryHeader";
 import "@/styles/globals.css";
 import { createTheme, ThemeProvider } from "@mui/material";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
 	const mainTheme = createTheme({
@@ -19,10 +21,18 @@ export default function App({ Component, pageProps }: AppProps) {
 		}
 	});
 
+	const router = useRouter();
+
 	return (
 		<>
 			<ThemeProvider theme={mainTheme}>
-				<Header />
+				{router.asPath === "/" ? (
+					<Header />
+				) : router.asPath === "/GestPage" ? (
+					<SecondaryHeader />
+				) : (
+					<></>
+				)}
 				<Component {...pageProps} />
 			</ThemeProvider>
 		</>
