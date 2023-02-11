dc4d07134269ead74c11685bc06f528b1c98eea6
Yuta123456 <yuuta09090530@icloud.com>
2023-02-11 16:48:44 +0900

    notistack作成

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,29 +2,38 @@ import { Header } from "@/components/Header";
 import "@/styles/globals.css";
 import { createTheme, ThemeProvider } from "@mui/material";
 import type { AppProps } from "next/app";
+import { SnackbarProvider } from "notistack";
 
 export default function App({ Component, pageProps }: AppProps) {
-	const mainTheme = createTheme({
-		typography: {
-			fontFamily: ["Roboto", '"Noto Sans JP"', '"Helvetica"', "Arial", "sans-serif"].join(",")
-		},
-		palette: {
-			primary: {
-				main: "#006A71",
-				light: "#46989F"
-			},
-			secondary: {
-				main: "#FFFFDD"
-			}
-		}
-	});
+  const mainTheme = createTheme({
+    typography: {
+      fontFamily: [
+        "Roboto",
+        '"Noto Sans JP"',
+        '"Helvetica"',
+        "Arial",
+        "sans-serif",
+      ].join(","),
+    },
+    palette: {
+      primary: {
+        main: "#006A71",
+        light: "#46989F",
+      },
+      secondary: {
+        main: "#FFFFDD",
+      },
+    },
+  });
 
-	return (
-		<>
-			<ThemeProvider theme={mainTheme}>
-				<Header />
-				<Component {...pageProps} />
-			</ThemeProvider>
-		</>
-	);
+  return (
+    <>
+      <ThemeProvider theme={mainTheme}>
+        <SnackbarProvider>
+          <Header />
+          <Component {...pageProps} />
+        </SnackbarProvider>
+      </ThemeProvider>
+    </>
+  );
 }
