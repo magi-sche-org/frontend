a424a3dd42928e928d176729cc856b8d3f78d033
Yuta123456 <yuuta09090530@icloud.com>
2023-02-11 16:29:48 +0900

    十分なmockMethodを作成

--- mockserver/server.js
+++ mockserver/server.js
@@ -1,16 +1,29 @@
-const PROTO_PATH = "protocol" + "/authorize.proto";
+const PROTO_AUTH = "protocol" + "/authorize.proto";
+const PROTO_EVENT = "protocol" + "/event.proto";
 
 const grpc = require("grpc");
 const protoLoader = require("@grpc/proto-loader");
-const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
+const packageAuthDefinition = protoLoader.loadSync(PROTO_AUTH, {
   keepCase: true,
   longs: String,
   enums: String,
   defaults: true,
   oneofs: true,
 });
-const protoDescriptor = grpc.loadPackageDefinition(packageDefinition);
-const authorize = protoDescriptor.geekCamp;
+
+const packageEventDefinition = protoLoader.loadSync(PROTO_EVENT, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+});
+
+const protoDescriptorAuth = grpc.loadPackageDefinition(packageAuthDefinition);
+const serviceAuth = protoDescriptorAuth.geekCamp;
+
+const protoDescriptorEvent = grpc.loadPackageDefinition(packageEventDefinition);
+const serviceEvent = protoDescriptorEvent.geekCamp;
 
 function doGetToken(call, callback) {
   callback(null, {
@@ -18,11 +31,35 @@ function doGetToken(call, callback) {
     status: 200,
   });
 }
+// string                   id       = 1;  // イベントid
+// string                   name     = 2;  // イベント名
+// bool                     owner    = 3;  // イベント所有者か
+// google.protobuf.Duration timeUnit = 4;  // 時間単位(秒)
+// google.protobuf.Duration duration = 5;  // 所要時間
+// repeated Answer          answers  = 6;  // 参加者の解答
+function doGetEvent(call, callback) {
+  callback(null, {
+    id: "hoge",
+    name: "イベントモック",
+    owner: false,
+    timeUnit: null,
+    duration: null,
+    answers: [],
+  });
+}
+
+function doRegisterAnswer(call, callback) {
+  callback(null, {})
+}
 
 function getServer() {
   const server = new grpc.Server();
-  server.addService(authorize.Authorize.service, {
+  server.addService(serviceAuth.Authorize.service, {
     GetToken: doGetToken,
+  });
+  server.addService(serviceEvent.Event.service, {
+    GetEvent: doGetEvent,
+    RegisterAnswer: doRegisterAnswer,
   });
   return server;
 }
