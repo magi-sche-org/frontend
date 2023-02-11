b6d74f13dd676011262cd2c7dcc219b46d2713bd
Yuta123456 <yuuta09090530@icloud.com>
2023-02-11 14:47:40 +0900

    test

--- src/components/TopPage/EventList.tsx
+++ src/components/TopPage/EventList.tsx
@@ -2,9 +2,11 @@ import { Box, Card } from "@mui/material";
 import { mockEventList } from "./data";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-import { FC } from "react";
+import { FC, useEffect } from "react";
 import { EventClient } from "@/service/api-client/protocol/EventServiceClientPb";
 import { GetEventRequest } from "@/service/api-client/protocol/event_pb";
+import { AuthorizeClient } from "@/service/api-client/protocol/AuthorizeServiceClientPb";
+import { GetTokenRequest } from "@/service/api-client/protocol/authorize_pb";
 export type Event = {
   id: number;
   name: string;
@@ -13,14 +15,13 @@ export type Event = {
 export const EventList = () => {
   // TODO: LocalStorageから取得
   const data = mockEventList;
-  const client = new EventClient("http://localhost:9000", null, null);
-  const req = new GetEventRequest();
-  req.setId("hoge");
-  req.setToken("fuga");
-  const eventData = client.getEvent(req, null).then((res) => {
-    console.log(res);
-    return res.getId();
-  });
+  useEffect(() => {
+    const client = new AuthorizeClient("http://localhost:9000", null, null);
+    const req = new GetTokenRequest();
+    const token = client.getToken(req, null).then((res) => {
+      console.log(res);
+    });
+  }, []);
   return (
     <>
       {data.map((event) => {
