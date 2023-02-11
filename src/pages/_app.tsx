7ef5c717e1e49ffd4c7226c9deb8cbf90ec8faef
Yuta123456 <yuuta09090530@icloud.com>
2023-02-11 14:49:58 +0900

    Revert "mswを導入"
    This reverts commit b8bc0aece7fc74e60e2473faf7bd620bfed5b85f.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,6 @@ import { Header } from "@/components/Header";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
-if (process.env.NODE_ENV === "development") {
-  // dynamic import でファイルを読み込んで MSW を有効にする
-  const MockServer = () =>
-    import("../mocks/browser").then((mo) => {
-      mo.worker.start();
-    });
-  MockServer();
-}
-
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
