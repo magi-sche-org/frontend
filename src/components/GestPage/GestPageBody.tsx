6fe01d57026f101ac7a717818ee580760d2db755
Yuma Satake <satous.y@icloud.com>
2023-02-12 13:05:52 +0900

    スタイル微調整

--- src/components/GestPage/GestPageBody.tsx
+++ src/components/GestPage/GestPageBody.tsx
@@ -1,7 +1,6 @@
 import {
   Box,
   Checkbox,
-  IconButton,
   Table,
   TableBody,
   TableCell,
@@ -9,7 +8,7 @@ import {
   TableHead,
   TableRow,
   TextField,
-  Typography,
+  Typography
 } from "@mui/material";
 import React from "react";
 import { Stack } from "@mui/system";
@@ -24,7 +23,7 @@ import { createProposedScheduleList } from "./proposedSchedule";
 import {
   Answer,
   GetEventResponse,
-  RegisterAnswerRequest,
+  RegisterAnswerRequest
 } from "../../service/api-client/protocol/event_pb";
 import { eventClient } from "../../service/api-client/client";
 import { Timestamp } from "google-protobuf/google/protobuf/timestamp_pb";
@@ -69,7 +68,7 @@ const GuestPageBody: FC<GuestPageBodyProps> = ({ eventDetail }) => {
       .then(() => {
         enqueueSnackbar("回答を記録しました。", {
           autoHideDuration: 2000,
-          variant: "success",
+          variant: "success"
         });
         // イベント入れる
         setEventToLocalStorage(eventDetail.name, eventDetail.id, localStorage);
@@ -81,13 +80,13 @@ const GuestPageBody: FC<GuestPageBodyProps> = ({ eventDetail }) => {
     <>
       <UserCalender />
       {/* タイトル・名前入力 */}
-      <Stack direction="column" sx={{ p: 3 }}>
-        <Typography variant="h6" sx={{ textAlign: "center", mb: 3 }}>
+      <Stack direction='column' sx={{ p: 3 }}>
+        <Typography variant='h6' sx={{ textAlign: "center", mb: 3 }}>
           {eventData.EventTitle}
         </Typography>
         <TextField
-          label="表示名"
-          variant="outlined"
+          label='表示名'
+          variant='outlined'
           sx={{ mx: 7, mb: 3 }}
           value={NameText}
           onChange={(e) => {
@@ -100,17 +99,17 @@ const GuestPageBody: FC<GuestPageBodyProps> = ({ eventDetail }) => {
             border: "solid",
             borderWidth: 0.3,
             borderRadius: 5,
-            p: 1,
+            p: 1
           }}
         >
           <Table>
             <TableHead>
               <TableRow>
                 <TableCell>
-                  <Typography variant="caption">日時</Typography>
+                  <Typography variant='caption'>日時</Typography>
                 </TableCell>
                 <TableCell>
-                  <Typography variant="caption">参加</Typography>
+                  <Typography variant='caption'>参加</Typography>
                 </TableCell>
               </TableRow>
             </TableHead>
@@ -119,10 +118,10 @@ const GuestPageBody: FC<GuestPageBodyProps> = ({ eventDetail }) => {
                 return (
                   <TableRow key={event.key}>
                     <TableCell>
-                      <Typography variant="body1">
-                        {event.startTime.getMonth() + 1} /{" "}
-                        {event.startTime.getDay()} {event.startTime.getHours()}:
-                        {event.startTime.getMinutes()}〜
+                      <Typography variant='body1'>
+                        {event.startTime.getMonth() + 1}&thinsp;/&thinsp;{event.startTime.getDay()}
+                        &emsp;
+                        {event.startTime.getHours()}:{event.startTime.getMinutes()}〜
                         {event.endTime.getHours()}:{event.endTime.getMinutes()}
                       </Typography>
                     </TableCell>
@@ -145,7 +144,7 @@ const GuestPageBody: FC<GuestPageBodyProps> = ({ eventDetail }) => {
         {/* 未ログイン */}
         <Box sx={{ m: 3 }}>
           {!LoginFlg ? (
-            <Typography variant="caption" sx={{ textAlign: "center" }}>
+            <Typography variant='caption' sx={{ textAlign: "center" }}>
               ※未ログインの為、後から予定を編集することができませんがよろしいでしょうか？
             </Typography>
           ) : (
@@ -153,8 +152,8 @@ const GuestPageBody: FC<GuestPageBodyProps> = ({ eventDetail }) => {
           )}
         </Box>
         {/* 決定 */}
-        <Stack direction="row" justifyContent="center" sx={{ mx: 15 }}>
-          <Button text="決定" isPrimary={true} onClick={Submit} />
+        <Stack direction='row' justifyContent='center' sx={{ mx: 15 }}>
+          <Button text='決定' isPrimary={true} onClick={Submit} />
         </Stack>
       </Stack>
     </>
