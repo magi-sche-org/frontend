36504d0e3947f6e34e56b5b059a247d6b355d333
xpa <xpadev@gmail.com>
2023-02-12 12:15:47 +0900

    fix: token utils

--- src/libraries/token.ts
+++ src/libraries/token.ts
@@ -2,16 +2,16 @@ import { AuthorizeClient } from "@/service/api-client/protocol/AuthorizeServiceC
 import { go_endpoint } from "@/libraries/env";
 import { GetTokenRequest } from "@/service/api-client/protocol/authorize_pb";
 
-const setToken = async () => {
+const fetchToken = async () => {
   const client = new AuthorizeClient(go_endpoint);
   const req = new GetTokenRequest();
   const res = await client.getToken(req, null);
   localStorage.setItem("go_token", res.getToken());
 };
 
-export { setToken };
-
-export const getToken = (localStorage: Storage) => {
+const getToken = (localStorage: Storage) => {
   const token = localStorage.getItem("go_token");
   return token || "";
 };
+
+export { getToken, fetchToken };
