66bd826d632f8fb227b83da4d97d4e1b048a390b
Yuma Satake <satous.y@icloud.com>
2023-03-02 20:28:35 +0900

    スタイル調整

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,26 +2,27 @@ import { Button as MUIButton } from "@mui/material";
 import { FC } from "react";
 
 type ButtonProps = {
-	text: string;
-	isPrimary: boolean;
-	onClick: () => void;
+  text: string;
+  isPrimary: boolean;
+  onClick: () => void;
 };
 export const Button: FC<ButtonProps> = ({ text, isPrimary, onClick }) => {
-	return (
-		<MUIButton
-			fullWidth
-			variant="outlined"
-			sx={{
-				background: isPrimary ? "#006A71" : "white",
-				padding: "12px",
-				color: isPrimary ? "white" : "black",
-				// ちょっとかなり微妙
-				// border: "none",
-				":hover": { background: isPrimary ? "#006A71" : "white" },
-			}}
-			onClick={onClick}
-		>
-			{text}
-		</MUIButton>
-	);
+  return (
+    <MUIButton
+      fullWidth
+      variant='outlined'
+      sx={{
+        borderRadius: 2,
+        background: isPrimary ? "#006A71" : "white",
+        p: 1.5,
+        color: isPrimary ? "white" : "black",
+        // ちょっとかなり微妙
+        // border: "none",
+        ":hover": { background: isPrimary ? "#006A71" : "white" }
+      }}
+      onClick={onClick}
+    >
+      {text}
+    </MUIButton>
+  );
 };
