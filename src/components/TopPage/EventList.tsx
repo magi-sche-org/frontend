f8a69957821d81adc39b932b0589b2831e810151
Yuta123456 <yuuta09090530@icloud.com>
2023-03-02 10:20:38 +0900

    使ってないimport削除

--- src/components/TopPage/EventList.tsx
+++ src/components/TopPage/EventList.tsx
@@ -1,15 +1,7 @@
-import { Box, Card, Stack } from "@mui/material";
-import { mockEventList } from "./data";
-import CardContent from "@mui/material/CardContent";
-import Typography from "@mui/material/Typography";
+import { Stack } from "@mui/material";
 import { FC, useEffect, useState } from "react";
-import { EventClient } from "@/service/api-client/protocol/EventServiceClientPb";
-import { GetEventRequest } from "@/service/api-client/protocol/event_pb";
-import { AuthorizeClient } from "@/service/api-client/protocol/AuthorizeServiceClientPb";
-import { GetTokenRequest } from "@/service/api-client/protocol/authorize_pb";
-import { authClient } from "@/service/api-client/client";
 import ListButton from "./ListButton";
-import {getEventStorage} from "@/libraries/eventStorage";
+import { getEventStorage } from "@/libraries/eventStorage";
 export type Event = {
   id: string;
   name: string;
@@ -19,7 +11,7 @@ export type Event = {
 export const EventList = () => {
   const [eventList, setEventList] = useState<Event[]>([]);
   useEffect(() => {
-    if (typeof window !== "object")return;
+    if (typeof window !== "object") return;
     setEventList(getEventStorage());
   }, []);
   return (
