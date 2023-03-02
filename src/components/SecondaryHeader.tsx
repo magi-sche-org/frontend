c1777a97eaa36ea5ff4bdf6fd5fa84b4bfe8e31c
Yuta123456 <yuuta09090530@icloud.com>
2023-03-02 11:40:13 +0900

    login logout処理

--- src/components/SecondaryHeader.tsx
+++ src/components/SecondaryHeader.tsx
@@ -1,4 +1,3 @@
-import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
@@ -6,33 +5,57 @@ import IconButton from "@mui/material/IconButton";
 import AccountCircle from "@mui/icons-material/AccountCircle";
 import { Container } from "@mui/system";
 import Head from "next/head";
+import { getUserInfo } from "@/libraries/userInfo";
+import { login, revokeToken } from "@/libraries/authorization";
+import LogoutIcon from "@mui/icons-material/Logout";
+import { FC, useState } from "react";
 
-export const SecondaryHeader: React.FC = () => {
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-
+export const SecondaryHeader: FC = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [isLogin, setIsLogin] = useState(getUserInfo() && true);
   const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    if (getUserInfo()) {
+      revokeToken().then(() => setIsLogin(false));
+    } else {
+      login();
+    }
     setAnchorEl(event.currentTarget);
   };
-
   return (
     <>
       <Head>
-        <meta name='theme-color' media='(prefers-color-scheme: light)' content='#006A71' />
+        <meta
+          name="theme-color"
+          media="(prefers-color-scheme: light)"
+          content="#006A71"
+        />
       </Head>
       <AppBar
-        position='static'
+        position="static"
         sx={{ backgroundColor: "primary.main", boxShadow: 0, px: 2, py: 0.8 }}
       >
         <Container disableGutters>
           <Toolbar sx={{ xs: "flex" }}>
-            <IconButton edge='start' size='large' disabled>
+            <IconButton edge="start" size="large" disabled>
               <AccountCircle sx={{ color: "primary.main" }} />
             </IconButton>
-            <Typography variant='h5' sx={{ mx: "auto", fontFamily: "robots", fontWeight: "bold" }}>
+            <Typography
+              variant="h5"
+              sx={{ mx: "auto", fontFamily: "robots", fontWeight: "bold" }}
+            >
               Magi-Sche
             </Typography>
-            <IconButton edge='end' size='large' color='inherit' onClick={handleMenu}>
-              <AccountCircle />
+            <IconButton
+              edge="end"
+              size="large"
+              color="inherit"
+              onClick={handleMenu}
+            >
+              {isLogin ? (
+                <LogoutIcon></LogoutIcon>
+              ) : (
+                <AccountCircle sx={{ color: "black" }} />
+              )}
             </IconButton>
           </Toolbar>
         </Container>
