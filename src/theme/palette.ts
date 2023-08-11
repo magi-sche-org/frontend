feda4091a4e0c711f201a533393200339f1c7f68
Yuma Satake <satous.y@icloud.com>
2023-08-11 12:36:37 +0900

    feat: theme調整

---
+++ src/theme/palette.ts
@@ -0,0 +1,11 @@
+import { PaletteOptions } from "@mui/material";
+
+export const palette: PaletteOptions = {
+  primary: {
+    main: "#006A71",
+    light: "#46989F",
+  },
+  secondary: {
+    main: "#FFFFDD",
+  },
+};
---
+++ src/theme/typography.ts
@@ -0,0 +1,6 @@
+import { TypographyOptions } from "@mui/material/styles/createTypography";
+import { Quicksand } from "@next/font/google";
+
+export const typography: TypographyOptions = {
+  fontFamily: ["Quicksand", "sans-serif"].join(","),
+};
