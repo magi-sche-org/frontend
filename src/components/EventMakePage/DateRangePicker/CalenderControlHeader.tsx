ce402020ab949bdd72ca2c580af4993853b2b4ac
Yuma Satake <satous.y@icloud.com>
2023-09-19 17:02:14 +0900

    feat: DateRangePicker値のハンドリング実装

---
+++ src/components/EventMakePage/DateRangePicker/CalenderControlHeader.tsx
@@ -0,0 +1,55 @@
+import { Grid, Typography, IconButton } from "@mui/material";
+import { Stack } from "@mui/system";
+import { FC } from "react";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+
+type Props = {
+  headerLabel: string;
+  addMonth: () => void;
+  subtractMonth: () => void;
+};
+
+/**
+ * カレンダーの操作・表示部
+ *
+ * @param headerLabel 年月のラベル
+ * @param addMonth 月を進めるhandler
+ * @param subtractMonth 月を戻すhandler
+ */
+export const CalenderControlHeader: FC<Props> = ({
+  headerLabel,
+  addMonth,
+  subtractMonth,
+}) => {
+  return (
+    <Grid container columns={7} alignItems="center">
+      <Grid item xs={3}>
+        <Typography
+          variant="h6"
+          sx={{
+            ml: 2,
+          }}
+        >
+          {headerLabel}
+        </Typography>
+      </Grid>
+      <Grid item xs={2} />
+      <Grid item xs={2}>
+        <Stack
+          direction="row"
+          justifyContent="center"
+          alignItems="end"
+          spacing={2}
+        >
+          <IconButton onClick={subtractMonth}>
+            <ArrowBackIosIcon fontSize="small" />
+          </IconButton>
+          <IconButton onClick={addMonth}>
+            <ArrowForwardIosIcon fontSize="small" />
+          </IconButton>
+        </Stack>
+      </Grid>
+    </Grid>
+  );
+};
