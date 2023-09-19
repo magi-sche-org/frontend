7807b2969bf8c749457c0e15450b3e9fedb3b314
Yuma Satake <satous.y@icloud.com>
2023-09-19 17:01:58 +0900

    feat: DateRangePicker月管理実装

---
+++ src/components/EventMakePage/DateRangePicker/DateRangePicker.tsx
@@ -0,0 +1,107 @@
+import { Stack } from "@mui/system";
+import { Dayjs } from "dayjs";
+import { FC } from "react";
+import { CalenderHeader } from "./CalenderHeader";
+import { CalenderBody } from "./CalenderBody";
+import { CalenderItemType } from "@/@types/calender";
+import { useCalenderControl } from "@/hooks/useCalenderControl";
+import { CalenderControlHeader } from "./CalenderControlHeader";
+import _isBetween from "dayjs/plugin/isBetween";
+
+type Props = {
+  startDay: Dayjs | undefined;
+  endDay: Dayjs | undefined;
+  setStartDay: (newValue: Dayjs | undefined) => void;
+  setEndDay: (newValue: Dayjs | undefined) => void;
+};
+
+/**
+ * 日付間の開始日・終了日をカレンダーから選択
+ *
+ * @param startDay 開始日
+ * @param endDay 終了日
+ * @param setStartDay 開始日を更新するhandler
+ * @param setEndDay 終了日を更新するhandler
+ */
+export const DateRangePicker: FC<Props> = ({
+  startDay,
+  endDay,
+  setStartDay,
+  setEndDay,
+}) => {
+  const { nowDate, dateArrayByWeek, addMonth, subtractMonth } =
+    useCalenderControl();
+
+  // startDay・endDayがどちらか一方が存在しない
+  const isUndefinedSelectedDay = startDay === undefined || endDay === undefined;
+
+  /**
+   * startDayとendDayの選択handler
+   * @param day 選択された日付
+   *
+   * - 開始日・終了日が既に存在する場合：開始日を設定し、終了日を削除
+   * - 開始日のみが存在する場合：終了日を設定
+   * - 終了日に開始日より前の日付が設定される場合：開始日と終了日を入れ替える
+   */
+  const handleSelectDay = (day: Dayjs) => {
+    if (!isUndefinedSelectedDay) {
+      setStartDay(day);
+      setEndDay(undefined);
+    } else if (endDay === undefined) {
+      if (day > startDay!) {
+        setEndDay(day);
+      } else {
+        setEndDay(startDay);
+        setStartDay(day);
+      }
+    }
+  };
+
+  /**
+   * CalenderItemTypeに変換する
+   * - isStarted: startDayでtrue
+   * - isEnded: endDayでtrue
+   * - isBetween: startDay < dayNumber < endDay でtrue
+   */
+  const calenderArray: CalenderItemType[][] = dateArrayByWeek.map(
+    (weekArray) => {
+      return weekArray.map((day) => {
+        const dayStr = day.format("YYYY-MM-DD");
+        const startDayStr = startDay?.format("YYYY-MM-DD") ?? "";
+        const endDayStr = endDay?.format("YYYY-MM-DD") ?? "";
+        const isUndefinedSelectedDay = startDayStr === "" || endDayStr === "";
+
+        const isThisMonthDay = day.month() === nowDate.month();
+        const isStarted = dayStr === startDayStr;
+        const isEnded = dayStr === endDayStr;
+        const isBetween = isUndefinedSelectedDay
+          ? false
+          : day.isBetween(startDay, endDay);
+
+        return {
+          day,
+          isThisMonthDay,
+          isStarted,
+          isEnded,
+          isBetween,
+        };
+      });
+    },
+  );
+
+  return (
+    <Stack justifyContent="center" spacing={1}>
+      <CalenderControlHeader
+        headerLabel={nowDate.format("YYYY年MM月")}
+        addMonth={addMonth}
+        subtractMonth={subtractMonth}
+      />
+      <CalenderHeader />
+      <CalenderBody
+        calenderArray={calenderArray}
+        handleSelectDay={handleSelectDay}
+        isUndefinedSelectedDay={isUndefinedSelectedDay}
+      />
+    </Stack>
+  );
+};
